cda7c447dea40e0e7e8adb89019c1ebf424bab87
Gian Piero Canevari <canevarigian@gmail.com>
2024-10-10 21:52:34 +0200

    update ex

--- Testing.js
+++ Testing.js
@@ -146,9 +146,43 @@ for (let i = 0; i < 5; i++ ){
 
 console.log("Hey guys, I brought a:" ,drinkTray)   
 
-/** Week 2 */
+/** Week 2 - Taffic Light */
+
+// Week 1
 
 "use strict";
+/**
+ * The `state` property says what the traffic light's state (i.e. colour) is at
+ * that moment.
+ */
+const trafficLight = {
+  state: "green",
+};
+
+let rotations = 0;
+while (rotations < 2) {
+  const currentState = trafficLight.state;
+  console.log("The traffic light is on", currentState);
+
+  // TODO
+  // if the color is green, turn it orange
+  // if the color is orange, turn it red
+  // if the color is red, add 1 to rotations and turn it green
+}
+
+/**
+ * The output should be:
+
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+
+*/
+
+// "use strict";
 /**
  * The `trafficLight` object is now no longer a global variable. Instead,
  * it is defined in function `main()` and passed as a parameter to other
