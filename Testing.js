7fb2c3a5b05b2e8448b1681bba5d0bf8287adb07
Gian Piero Canevari <canevarigian@gmail.com>
2024-10-24 21:01:45 +0200

    week 3 js

--- Testing.js
+++ Testing.js
@@ -286,7 +286,7 @@ console.log(values1)
 function runExperiment(sampleSize) {
   const valueCounts = [0, 0, 0, 0, 0, 0];
 
-  // NEEDS TO BE FIXED
+  // NEEDS TO BE FIXE
   function getRandomIntInclusive(min=1, max=6) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
@@ -345,3 +345,41 @@ function main() {
 const sampleSizes = [100, 1000, 1000000];
 main();
 runExperiment(sampleSizes)
+
+// Week 3
+
+/**
+ * In the list below you have an array of numbers. The goal is to find the two numbers that add up to 2020.
+
+Once you have found those numbers, multiply the numbers and store the result of that in the result variable.
+ */
+
+
+const list1 = [1721, 979, 366, 299, 675, 1456];
+let result;
+let sum = 0
+
+const total = 2020
+// Write your code here
+for (let i = 0; i < list1.length; i++)  {
+  for (let j = 0; j < list1[i].length; j++)  {
+    const suma = i+j 
+    console.log(i, j , suma)
+    if ( suma === total)  {
+     result = i * j 
+     console.log(result)
+    }
+  }
+}
+
+// list1.map((firstValue)=>{
+//   list1.map((secondValue)=>{
+//     const suma = firstValue + secondValue
+//     if(suma=== total){
+//       result = i * j 
+//     }
+//   }) 
+// })
+
+// TEST CODE, do not change
+console.assert(result === 514579, `The result is not correct, it is ${result}, but should be 514579`);
