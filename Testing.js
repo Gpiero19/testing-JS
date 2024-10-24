476d6c05e0518aa010143fe46cff839112fe7ccf
Gian Piero Canevari <canevarigian@gmail.com>
2024-10-24 22:03:28 +0200

    password validation part1

--- Testing.js
+++ Testing.js
@@ -386,3 +386,63 @@ for (let i = 0; i < list1.length; i++)  {
 // console.assert(result === 514579, `The result is not correct, it is ${result}, but should be 514579`);
 
 console.assert(result === 241861950, `The result is not correct, it is ${result}, but should be 241861950`);
+
+
+/**
+ * Credit to https://adventofcode.com/ for this exercise
+ * 
+ * Each object in the passwordList gives a password policy and then the password.
+ * The times field says the minimal and maximal amount of times the letter should be in the password. So 1-3 means at least 1 time, at most 3 times.
+ * The letter field gives which letter should be counted
+ * The password field gives the password
+ * 
+ * In the list 2 passwords are valid, the middle one is not as there is no b in the password.
+ * 
+ * We expect the output:
+ * 
+ * 'abcde' is VALID, a is present 1 times and should have been present at least 1 and at most 3 times
+ * 'cdefg' is INVALID, b is present 0 times and should have been present at least 1 and at most 3 times
+ * 'ccccccccc' is VALID, c is present 9 times and should have been present at least 2 and at most 9 times
+ */
+
+const passwordList = [
+  { times: '1-3', letter: 'a', password: 'abcde'},
+  { times: '1-3', letter: 'b', password: 'cdefg'},
+  { times: '2-9', letter: 'c', password: 'ccccccccc'}
+];
+
+const parseTimesToNumber = (time = "")=> {
+
+   const min = Number(time.split("-")[0])
+  const max =  Number(time.split("-")[1] )
+
+    return {
+      min,
+      max
+    }
+}
+
+
+
+const number = parseTimesToNumber(passwordList[0].times)
+const letter = passwordList[0].letter
+const password =  passwordList[0].password
+
+let count = 0 
+const listnumber = password.split("")
+
+
+listnumber.map((char)=>{
+  if(char === letter){
+    count + 1
+  }
+})
+
+if(count <= number.max || count >= number.min){
+
+  console.log("YEEES")
+}
+
+console.log(listnumber)
+// for (i = 0; passwordList[i])
+// Object.keys(password.)
